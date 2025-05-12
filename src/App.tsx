d5ef3c53370b0801c184f932c7a0748ccc5051ff
branxy <churkin.michael@gmail.com>
2025-05-12 15:17:47 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen grid place-items-center">
+      <h1 className="text-3xl font-oswald text-center font-extralight">
+        React Vite starter
+      </h1>
+    </div>
+  )
+}
+
+export default App
